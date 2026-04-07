d94262f24a2def568e229492326c2bc1a5febbee
Ryan Hurley <ryanhurleyv2624066@users.noreply.github.com>
2026-04-08 04:27:02 +0800

    feat: add get_version() helper to expose package version

--- src/stackwatch/__init__.py
+++ src/stackwatch/__init__.py
@@ -3,3 +3,18 @@
 __version__ = "0.1.0"
 __author__ = "stackwatch contributors"
 __description__ = "Monitor AWS CloudFormation stack events in real-time with filtered output and alerting."
+
+
+def get_version() -> str:
+    """Return the current version of stackwatch.
+
+    Returns:
+        str: The version string in PEP 440 format (e.g. ``"0.1.0"``).
+
+    Example::
+
+        >>> import stackwatch
+        >>> stackwatch.get_version()
+        '0.1.0'
+    """
+    return __version__
